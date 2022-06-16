741762bab39aba2abe482ade78dd741fd62c91d5
Felix Uhl <felix.uhl@outlook.com>
2022-06-16 17:02:06 +0200

    Dispatch pin updates via dedicated struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
+use core::cell::Cell;
+
 use arduino_hal::{
     hal::port::{PB5, PD2, PD3, PD4, PD5, PD6, PD7},
     port::{
-        mode::{Input, Output, PullUp},
+        mode::{Floating, Input, Output, PullUp},
         Pin,
     },
 };
@@ -34,70 +36,60 @@ where
         IOPin::Output(output_pin)
     }
 
-    fn input(self) -> (Self, bool) {
+    fn input(self, is_high: &mut bool) -> Self {
         let input_pin = match self {
             IOPin::Input(input_pin) => input_pin,
             IOPin::Output(output_pin) => output_pin.into_pull_up_input(),
         };
-        let is_high = input_pin.is_high();
-        (IOPin::Input(input_pin), is_high)
+        *is_high = input_pin.is_high();
+        IOPin::Input(input_pin)
     }
 }
 
-enum UnoPin {
-    D13(IOPin<PB5>),
-    D2(IOPin<PD2>),
-    D3(IOPin<PD3>),
-    D4(IOPin<PD4>),
-    D5(IOPin<PD5>),
-    D6(IOPin<PD6>),
-    D7(IOPin<PD7>),
+fn new_pin<T>(pin: Pin<Input<Floating>, T>) -> Cell<Option<IOPin<T>>>
+where
+    T: avr_hal_generic::port::PinOps,
+{
+    Cell::new(Some(IOPin::Input(pin.into_pull_up_input())))
 }
 
-impl UnoPin {
-    fn output_state(self, state: PinState) -> Self {
-        match self {
-            Self::D13(io_pin) => Self::D13(io_pin.output_state(state)),
-            Self::D2(io_pin) => Self::D2(io_pin.output_state(state)),
-            Self::D3(io_pin) => Self::D3(io_pin.output_state(state)),
-            Self::D4(io_pin) => Self::D4(io_pin.output_state(state)),
-            Self::D5(io_pin) => Self::D5(io_pin.output_state(state)),
-            Self::D6(io_pin) => Self::D6(io_pin.output_state(state)),
-            Self::D7(io_pin) => Self::D7(io_pin.output_state(state)),
-        }
+struct PinDispatcher {
+    d13: Cell<Option<IOPin<PB5>>>,
+    d2: Cell<Option<IOPin<PD2>>>,
+    d3: Cell<Option<IOPin<PD3>>>,
+    d4: Cell<Option<IOPin<PD4>>>,
+    d5: Cell<Option<IOPin<PD5>>>,
+    d6: Cell<Option<IOPin<PD6>>>,
+    d7: Cell<Option<IOPin<PD7>>>,
+}
+
+impl PinDispatcher {
+    fn output(&self, pin_label: char, state: PinState) {
+        match pin_label {
+            '1' => self.d13.set(Some(self.d13.take().unwrap().output_state(state))),
+            '2' => self.d2.set(Some(self.d2.take().unwrap().output_state(state))),
+            '3' => self.d3.set(Some(self.d3.take().unwrap().output_state(state))),
+            '4' => self.d4.set(Some(self.d4.take().unwrap().output_state(state))),
+            '5' => self.d5.set(Some(self.d5.take().unwrap().output_state(state))),
+            '6' => self.d6.set(Some(self.d6.take().unwrap().output_state(state))),
+            '7' => self.d7.set(Some(self.d7.take().unwrap().output_state(state))),
+            _ => unreachable!(),
+        };
     }
 
-    fn input(self) -> (Self, bool) {
-        match self {
-            Self::D13(io_pin) => {
-                let (pin, is_high) = io_pin.input();
-                (Self::D13(pin), is_high)
-            }
-            Self::D2(io_pin) => {
-                let (pin, is_high) = io_pin.input();
-                (Self::D2(pin), is_high)
-            }
-            Self::D3(io_pin) => {
-                let (pin, is_high) = io_pin.input();
-                (Self::D3(pin), is_high)
-            }
-            Self::D4(io_pin) => {
-                let (pin, is_high) = io_pin.input();
-                (Self::D4(pin), is_high)
-            }
-            Self::D5(io_pin) => {
-                let (pin, is_high) = io_pin.input();
-                (Self::D5(pin), is_high)
-            }
-            Self::D6(io_pin) => {
-                let (pin, is_high) = io_pin.input();
-                (Self::D6(pin), is_high)
-            }
-            Self::D7(io_pin) => {
-                let (pin, is_high) = io_pin.input();
-                (Self::D7(pin), is_high)
-            }
-        }
+    fn input(&mut self, pin_label: char) -> bool {
+        let mut is_high = false;
+        match pin_label {
+            '1' => self.d13.set(Some(self.d13.take().unwrap().input(&mut is_high))),
+            '2' => self.d2.set(Some(self.d2.take().unwrap().input(&mut is_high))),
+            '3' => self.d3.set(Some(self.d3.take().unwrap().input(&mut is_high))),
+            '4' => self.d4.set(Some(self.d4.take().unwrap().input(&mut is_high))),
+            '5' => self.d5.set(Some(self.d5.take().unwrap().input(&mut is_high))),
+            '6' => self.d6.set(Some(self.d6.take().unwrap().input(&mut is_high))),
+            '7' => self.d7.set(Some(self.d7.take().unwrap().input(&mut is_high))),
+            _ => unreachable!(),
+        };
+        is_high
     }
 }
 
@@ -118,13 +110,15 @@ fn main() -> ! {
      * examples available.
      */
 
-    let mut d2 = UnoPin::D2(IOPin::Input(pins.d2.into_pull_up_input()));
-    let mut d3 = UnoPin::D3(IOPin::Input(pins.d3.into_pull_up_input()));
-    let mut d4 = UnoPin::D4(IOPin::Input(pins.d4.into_pull_up_input()));
-    let mut d5 = UnoPin::D5(IOPin::Input(pins.d5.into_pull_up_input()));
-    let mut d6 = UnoPin::D6(IOPin::Input(pins.d6.into_pull_up_input()));
-    let mut d7 = UnoPin::D7(IOPin::Input(pins.d7.into_pull_up_input()));
-    let mut led = UnoPin::D13(IOPin::Input(pins.d13.into_pull_up_input()));
+    let mut pin_dispatcher = PinDispatcher {
+        d13: new_pin(pins.d13),
+        d2: new_pin(pins.d2),
+        d3: new_pin(pins.d3),
+        d4: new_pin(pins.d4),
+        d5: new_pin(pins.d5),
+        d6: new_pin(pins.d6),
+        d7: new_pin(pins.d7),
+    };
 
     loop {
         let mut pin: Option<char> = None;
@@ -147,31 +141,11 @@ fn main() -> ! {
         while serial.read_byte() as char != '\n' {
             ufmt::uwrite!(&mut serial, "Expecting newline. ").unwrap();
         }
-        if let (Some(pin), Some(action)) = (pin, action) {
+        if let (Some(pin_label), Some(action)) = (pin, action) {
             match action {
-                Action::Output(state) => match pin {
-                    '1' => led = led.output_state(state),
-                    '2' => d2 = d2.output_state(state),
-                    '3' => d3 = d3.output_state(state),
-                    '4' => d4 = d4.output_state(state),
-                    '5' => d5 = d5.output_state(state),
-                    '6' => d6 = d6.output_state(state),
-                    '7' => d7 = d7.output_state(state),
-                    _ => unreachable!(),
-                },
+                Action::Output(state) => pin_dispatcher.output(pin_label, state),
                 Action::Input => {
-                    let is_high;
-                    match pin {
-                        '1' => (led, is_high) = led.input(),
-                        '2' => (d2, is_high) = d2.input(),
-                        '3' => (d3, is_high) = d3.input(),
-                        '4' => (d4, is_high) = d4.input(),
-                        '5' => (d5, is_high) = d5.input(),
-                        '6' => (d6, is_high) = d6.input(),
-                        '7' => (d7, is_high) = d7.input(),
-                        _ => unreachable!(),
-                    };
-                    if is_high {
+                    if pin_dispatcher.input(pin_label) {
                         ufmt::uwrite!(&mut serial, "Pin is high. ").unwrap();
                     } else {
                         ufmt::uwrite!(&mut serial, "Pin is low. ").unwrap();
