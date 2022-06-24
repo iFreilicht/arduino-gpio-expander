91003b009f4bfddc67b7a5c060745a67aadc44ea
Felix Uhl <felix.uhl@outlook.com>
2022-06-24 16:50:03 +0200

    Move pin dispatching into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,16 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::hal::port::{
-    mode::{Floating, Input, Output, PullUp},
-    Pin,
-};
-use core::{cell::Cell, fmt};
-use embedded_hal::digital::v2::{OutputPin, PinState};
-use panic_halt as _;
+mod pins;
+use pins::PinDispatcher;
 
-use heapless::FnvIndexMap;
+use embedded_hal::digital::v2::PinState;
+use panic_halt as _;
 
 enum Action {
     Output(PinState),
     Input,
     List,
-}
-
-enum StatefulPin<T> {
-    Input(Pin<Input<PullUp>, T>),
-    Output(Pin<Output, T>),
-}
-
-impl<T> StatefulPin<T>
-where
-    T: avr_hal_generic::port::PinOps,
-{
-    fn output_state(self, state: PinState) -> Self {
-        let mut output_pin = match self {
-            StatefulPin::Input(input_pin) => input_pin.into_output(),
-            StatefulPin::Output(output_pin) => output_pin,
-        };
-        output_pin.set_state(state).unwrap();
-        StatefulPin::Output(output_pin)
-    }
-
-    fn input(self, is_high: &mut bool) -> Self {
-        let input_pin = match self {
-            StatefulPin::Input(input_pin) => input_pin,
-            StatefulPin::Output(output_pin) => output_pin.into_pull_up_input(),
-        };
-        *is_high = input_pin.is_high();
-        StatefulPin::Input(input_pin)
-    }
-}
-
-struct MutablePin<T> {
-    pin: Cell<Option<StatefulPin<T>>>,
-    name: &'static str,
-}
-
-impl<T> MutablePin<T>
-where
-    T: avr_hal_generic::port::PinOps,
-{
-    fn new(pin: Pin<Input<Floating>, T>, name: &'static str) -> Self {
-        Self {
-            pin: Cell::new(Some(StatefulPin::Input(pin.into_pull_up_input()))),
-            name,
-        }
-    }
-}
-
-impl<T> fmt::Debug for MutablePin<T>
-where
-    T: avr_hal_generic::port::PinOps,
-{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!("MutablePin {}", self.name))
-    }
-}
-
-trait IOPin: fmt::Debug {
-    fn output_state(&mut self, state: PinState);
-    fn input(&mut self) -> bool;
-    fn name(&self) -> &'static str;
-}
-
-impl<T> IOPin for MutablePin<T>
-where
-    T: avr_hal_generic::port::PinOps,
-{
-    fn output_state(&mut self, state: PinState) {
-        self.pin.set(Some(self.pin.take().unwrap().output_state(state)))
-    }
-
-    fn input(&mut self) -> bool {
-        let mut is_high = false;
-        self.pin.set(Some(self.pin.take().unwrap().input(&mut is_high)));
-        is_high
-    }
-
-    fn name(&self) -> &'static str {
-        self.name
-    }
-}
-
-type PinMap<'a> = FnvIndexMap<char, &'a mut dyn IOPin, 64>;
-
-struct PinDispatcher<'a> {
-    pin_map: PinMap<'a>,
-}
-
-impl<'a> PinDispatcher<'a> {
-    fn new() -> Self {
-        PinDispatcher { pin_map: PinMap::new() }
-    }
-
-    fn add_pin(&mut self, pin_label: char, pin: &'a mut dyn IOPin) {
-        let maybe_previous_pin = self.pin_map.insert(pin_label, pin).unwrap();
-        if maybe_previous_pin.is_some() {
-            panic!("Inserting pin failed because the pin_label was already in use.")
-        }
-    }
-
-    fn output(&mut self, pin_label: char, state: PinState) {
-        self.get_pin(pin_label).output_state(state);
-    }
-
-    fn input(&mut self, pin_label: char) -> bool {
-        self.get_pin(pin_label).input()
-    }
-
-    fn get_pin(&mut self, pin_label: char) -> &mut dyn IOPin {
-        *self.pin_map.get_mut(&pin_label).unwrap()
-    }
-}
-
-macro_rules! add_pin {
-    ($dispatcher:ident, $pins:ident.$name:ident, $tag:literal) => {
-        let mut $name = MutablePin::new($pins.$name, stringify!($name));
-        $dispatcher.add_pin($tag, &mut $name);
-    };
 }
 
 #[arduino_hal::entry]
