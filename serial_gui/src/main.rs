e48c1b9a9b83e6bab006285e979a125e89ff23a6
Felix Uhl <felix.uhl@outlook.com>
2022-06-25 15:05:08 +0200

    Add serial-gui binary crate
    Code is copied from https://github.com/emilk/eframe_template/.
    I skipped all the parts that enable compiling to WASM.

---
+++ serial_gui/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Box::new(serial_gui::TemplateApp::new(cc))),
+    );
+}
